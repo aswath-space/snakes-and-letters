ae3a81d2b32a37934bd7cdf39be9267f6be40dad
Aswath Subramanian <146985852+aswath-space@users.noreply.github.com>
2025-08-25 12:31:46 +0200

    Improve accessibility for input and toggle controls

--- src/components/ToggleBar.tsx
+++ src/components/ToggleBar.tsx
@@ -4,31 +4,35 @@ export default function ToggleBar() {
   const rules = useGameStore((s) => s.rules);
   const setRules = useGameStore((s) => s.newGame);
   return (
-    <div className="flex space-x-2">
-      <label>
+    <fieldset className="flex space-x-2">
+      <legend className="sr-only">Game options</legend>
+      <div className="flex items-center space-x-1">
         <input
+          id="toggle-challenge"
           type="checkbox"
           checked={rules.challengeMode}
           onChange={() => setRules({ challengeMode: !rules.challengeMode })}
         />
-        Challenge
-      </label>
-      <label>
+        <label htmlFor="toggle-challenge">Challenge</label>
+      </div>
+      <div className="flex items-center space-x-1">
         <input
+          id="toggle-no-repeats"
           type="checkbox"
           checked={rules.noRepeats}
           onChange={() => setRules({ noRepeats: !rules.noRepeats })}
         />
-        No Repeats
-      </label>
-      <label>
+        <label htmlFor="toggle-no-repeats">No Repeats</label>
+      </div>
+      <div className="flex items-center space-x-1">
         <input
+          id="toggle-timer"
           type="checkbox"
           checked={rules.timer}
           onChange={() => setRules({ timer: !rules.timer })}
         />
-        Timer
-      </label>
-    </div>
+        <label htmlFor="toggle-timer">Timer</label>
+      </div>
+    </fieldset>
   );
 }
